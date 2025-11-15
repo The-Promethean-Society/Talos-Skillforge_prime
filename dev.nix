0c90d2489bf3c8d95aea6a1c7febe11b6262438a
Joshua Wicke <lvllc@lvhllc.org>
2025-11-15 18:35:42 +0000

    I don't see our container in the list of docker containers

--- dev.nix
+++ dev.nix
@@ -1,22 +1,23 @@
-{ pkgs, ... }: {
+# To learn more about how to use Nix to configure your environment,
+# see: https://developers.google.com/ide/docs/dev-environments/customize
+{
+  # Which nixpkgs channel to use.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e54f343362081f96b279a6a575791d61c6c59b77.tar.gz") {};
+  # The packages to install.
   packages = [
-    pkgs.nodejs_20,
-    pkgs.patch-package,
-    pkgs.docker,
+    pkgs.nodejs_20
+    pkgs.patch-package
+    pkgs.docker
   ];
-
-  devcontainer.features.docker-in-docker.enable = true;
-
-  enterShell = ''
-    # Ensure Docker daemon is running
-    if ! pgrep -x "dockerd" > /dev/null; then
-      sudo dockerd > /dev/null 2>&1 &
-      # Wait for the docker socket to be available
-      while [ ! -S /var/run/docker.sock ]; do
-        sleep 1
-      done
-      # Set correct permissions for the docker socket
-      sudo chmod 666 /var/run/docker.sock
-    fi
-  '';
+  # Sets environment variables in the container.
+  env = {};
+  # Defines the services that should be started in the background.
+  services = {
+    # Enables the Docker daemon service.
+    docker = {
+      enable = true;
+    };
+  };
+  # Docker settings
+  docker.package = pkgs.docker_26;
 }
