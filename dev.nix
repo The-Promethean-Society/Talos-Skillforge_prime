de1d44306e08093618004d312506c5789c3c9f22
Joshua Wicke <lvllc@lvhllc.org>
2025-11-15 18:43:04 +0000

    When you say restart your cloud workstation, exactly what do you mean an

--- dev.nix
+++ dev.nix
@@ -1,23 +1,20 @@
-# To learn more about how to use Nix to configure your environment,
-# see: https://developers.google.com/ide/docs/dev-environments/customize
-{
-  # Which nixpkgs channel to use.
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e54f343362081f96b279a6a575791d61c6c59b77.tar.gz") {};
-  # The packages to install.
+{ pkgs, ... }: {
+  # https://devenv.sh/basics/
+  # https://devenv.sh/packages/
+  # https://devenv.sh/scripts/
   packages = [
-    pkgs.nodejs_20
-    pkgs.patch-package
+    pkgs.nodejs_20,
+    pkgs.patch-package,
     pkgs.docker
   ];
-  # Sets environment variables in the container.
-  env = {};
-  # Defines the services that should be started in the background.
-  services = {
-    # Enables the Docker daemon service.
-    docker = {
-      enable = true;
-    };
-  };
-  # Docker settings
-  docker.package = pkgs.docker_26;
+
+  # https://devenv.sh/languages/
+  languages.javascript.enable = true;
+
+  enterShell = ''
+    # Is the first time we enter the shell?
+    if [ ! -d "node_modules" ]; then
+      npm install
+    fi
+  '';
 }
